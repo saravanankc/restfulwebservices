98dd9a89c8494edd01af7e89f2ac3c8466aa5929
saravanannachiappan <snachiappan@solstice.com>
2020-03-06 11:34:14 -0600

    Internationalization: Locale Resolver, AcceptHeaderLocaleResolver example

--- src/main/java/com/kc/learnings/spring/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/kc/learnings/spring/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,14 @@ package com.kc.learnings.spring.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.el.ResourceBundleELResolver;
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,5 +17,28 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+
+	// Instead of SessionLocaleResolver use AcceptHeaderLocaleResolver for
+	// ingesting "Accept-Language" header on all rest call to support internationalization
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+		acceptHeaderLocaleResolver.setDefaultLocale(Locale.US);
+		return acceptHeaderLocaleResolver;
+	}
+
+	//	public LocaleResolver localeResolver(){
+//		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+//		sessionLocaleResolver.setDefaultLocale(Locale.US);
+//		return sessionLocaleResolver;
+//	}
+
+	//Instead of this bean, add a property "spring.messages.basename=messages" that will automatically take care of understanding the Message basedname.
+//	@Bean
+//	public ResourceBundleMessageSource bundleMessageSource(){
+//		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+//		resourceBundleMessageSource.setBasename("messages");
+//		return resourceBundleMessageSource;
+//	}
 
 }
--- src/main/java/com/kc/learnings/spring/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/kc/learnings/spring/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,18 @@
 package com.kc.learnings.spring.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+import java.util.Locale;
 
 //@Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     //@RequestMapping(method = RequestMethod.GET, path = "/hello-world") //Alternatively use @GetMapping
     @GetMapping(path = "/hello-world")
@@ -22,6 +30,17 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+//    @GetMapping(path = "/hello-world-internationalized")
+//    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale){
+//        return messageSource.getMessage("good.morning.message", null, locale);
+//    }
+
+
+    //Instead of pass locale parameter to controller method, user LocalContectHolder.getLocale() to automatically detect Accept-Language header
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+    }
 }
 
 
