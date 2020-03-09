c93f28ea8b8738c3dd7839b051fbb00fc8f84e4f
saravanannachiappan <snachiappan@solstice.com>
2020-03-09 18:19:10 -0500

    Added extended API documentation through SwaggerConfig.java and added addition API Model properties on User.java entity

--- src/main/java/com/kc/learnings/spring/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/kc/learnings/spring/restfulwebservices/SwaggerConfig.java
@@ -2,13 +2,62 @@ package com.kc.learnings.spring.restfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    /*To update below default API info documentation use ApiInfo object
+       "info": {
+        "description": "Api Documentation",
+                "version": "1.0",
+                "title": "Api Documentation",
+                "termsOfService": "urn:tos",
+                "contact": {},
+        "license": {
+            "name": "Apache 2.0",
+                    "url": "http://www.apache.org/licenses/LICENSE-2.0"
+        }
+    },
+    */
+
+    private static final Contact DEFAULT_CONTACT = new Contact(
+            "Rest Admin",
+            "http://kc.com",
+            "rest-admin@kc.com"
+    );
+    private static final Collection<VendorExtension> DEFAULT_VENDOR_EXTENSIONS = new ArrayList<>();
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Restful webservice API", //String title,
+            "KC Learning - restful webservice API", //String description,
+            "1.0", //String version,
+            "Some Terms and Conditions URL", //String termsOfServiceUrl,
+            DEFAULT_CONTACT, // Contact contact,
+            "Apache 2.0", //String license,
+            "http://www.apache.org/licenses/LICENSE-2.0", //String licenseUrl,
+            DEFAULT_VENDOR_EXTENSIONS
+    );
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>();
+
+    static {
+        DEFAULT_PRODUCES_AND_CONSUMES.add("application/xml");
+        DEFAULT_PRODUCES_AND_CONSUMES.add("application/json");
+    }
+
 
     //Bean - Docket
     // All paths
@@ -18,6 +67,10 @@ public class SwaggerConfig {
         //Enables following endpoints once configured
         //http://localhost:8080/swagger-ui.html
         //http://localhost:8080/v2/api-docs
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                //.apiInfo(ApiInfo.DEFAULT)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
--- src/main/java/com/kc/learnings/spring/restfulwebservices/user/User.java
+++ src/main/java/com/kc/learnings/spring/restfulwebservices/user/User.java
@@ -1,17 +1,23 @@
 package com.kc.learnings.spring.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user!")
 public class User {
 
     private int id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(name = "Name of user", example = "Swagger Admin user", notes = "Name should have at least 2 characters")
     private String name;
 
     @Past(message = "Birth date cannot be greater than or equal to current date")
+    @ApiModelProperty(name = "Name of user", example = "Swagger Admin user", notes = "Birth date cannot be greater than or equal to current date")
     private Date birthDate;
 
     public int getId() {
